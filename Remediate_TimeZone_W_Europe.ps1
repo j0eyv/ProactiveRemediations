f66b4d9b255dece8459269a5076f98614f45f213
j0eyv <41282854+j0eyv@users.noreply.github.com>
2022-09-29 09:21:29 +0200

    Add files via upload

--- Remediate_TimeZone_W_Europe.ps1
+++ Remediate_TimeZone_W_Europe.ps1
@@ -1,1 +1,1 @@
-﻿New-ItemProperty -LiteralPath "HKLM:\SSYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name "TimeZoneKeyName" -Value "W. Europe Standard Time" -PropertyType STRING -Force -ea SilentlyContinue;
+﻿tzutil /s "W. Europe Standard Time"
