44b460b3afaf54eb652701d116fdcff6e92b4b94
j0eyv <41282854+j0eyv@users.noreply.github.com>
2022-09-29 09:18:17 +0200

    Add files via upload

---
+++ Detect__TimeZone_W_Europe.ps1
@@ -0,0 +1,18 @@
+﻿$Path = "HKLM:\SSYSTEM\CurrentControlSet\Control\TimeZoneInformation"
+$Name = "TimeZoneKeyName"
+$Type = "STRING"
+$Value = "W. Europe Standard Time"
+
+Try {
+    $Registry = Get-ItemProperty -Path $Path -Name $Name -ErrorAction Stop | Select-Object -ExpandProperty $Name
+    If ($Registry -eq $Value){
+        Write-Output "Compliant"
+        Exit 0
+    } 
+    Write-Warning "Not Compliant"
+    Exit 1
+} 
+Catch {
+    Write-Warning "Not Compliant"
+    Exit 1
+}
---
+++ Remediate_TimeZone_W_Europe.ps1
@@ -0,0 +1,1 @@
+﻿New-ItemProperty -LiteralPath "HKLM:\SSYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name "TimeZoneKeyName" -Value "W. Europe Standard Time" -PropertyType STRING -Force -ea SilentlyContinue;
