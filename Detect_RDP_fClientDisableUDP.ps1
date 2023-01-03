6abc8f461b1ad61242f96eb10cb530dcb5eba25f
j0eyv <41282854+j0eyv@users.noreply.github.com>
2023-01-03 11:24:09 +0100

    Add files via upload

---
+++ Detect_RDP_fClientDisableUDP.ps1
@@ -0,0 +1,21 @@
+﻿$Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\Client"
+$Name = "fClientDisableUDP"
+$Type = "DWORD"
+$Value = 1
+
+Try {
+    $Registry = Get-ItemProperty -Path $Path -Name $Name -ErrorAction Stop | Select-Object -ExpandProperty $Name
+    If ($Registry -eq $Value){
+        Write-Output "Compliant"
+        Exit 0
+    } 
+    else
+    {
+    Write-Warning "Not Compliant"
+    Exit 1
+    }
+} 
+Catch {
+    Write-Warning "Not Compliant"
+    Exit 1
+}
---
+++ Remediate_RDP_fClientDisableUDP.ps1
@@ -0,0 +1,14 @@
+﻿$Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\Client"
+$Name = "fClientDisableUDP"
+$Type = "DWord"
+$Value = "1"
+
+IF(!(Test-Path $Path))
+  {
+    New-Item -Path $Path -Force | Out-Null
+    New-ItemProperty -Path $Path -Name $name -Value $value -PropertyType $Type -Force | Out-Null
+    }
+
+ELSE {
+    New-ItemProperty -Path $Path -Name $name -Value $value -PropertyType $Type -Force | Out-Null
+    }
