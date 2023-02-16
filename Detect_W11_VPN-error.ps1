13cb848411f9fbba31225936738a6d8d9376f60b
j0eyv <41282854+j0eyv@users.noreply.github.com>
2023-02-16 14:30:06 +0100

    Add files via upload

--- Detect_W11_VPN-error.ps1
+++ Detect_W11_VPN-error.ps1
@@ -1,9 +1,6 @@
 ﻿# Windows 11 and Intune are no a great combination. When deploying VPN connections to Windows 11 via Intune, they get corrupt. The command get-vpnconnection does result in a strange error.
 # Ths Proactive Remediation script checks if the VPN Profile is available. If not, an error will show and will trigger the remediation.
 
-# Note: This is stil under construction. The Get-VPNConnection cmdlet does not work in this state and thus it will run the remediation script. That uses the Get-VPNConnection cmdlet to remove the current profile. Still need to find a way to remove the VPN profile without using Get-VPNConnection.
-
-
 $VPNName = "AOVPN" # Write down the VPN Name as shown in the "Name" output.
 
 Try {
