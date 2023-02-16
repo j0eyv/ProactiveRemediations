660be51c0d0bc84ebf0424d45ecffe072c329b08
j0eyv <41282854+j0eyv@users.noreply.github.com>
2023-02-16 14:13:51 +0100

    Add files via upload

--- Remediate_W11_VPN-error.ps1
+++ Remediate_W11_VPN-error.ps1
@@ -1,6 +1,8 @@
 ﻿# The remediation will remove the VPN connection and trigger a Intune sync.
 # VP Connection should be available again in a few minutes.
 
+# Note: This is stil under construction. The Get-VPNConnection cmdlet does not work in this state and thus it will run the remediation script. That uses the Get-VPNConnection cmdlet to remove the current profile. Still need to find a way to remove the VPN profile without using Get-VPNConnection.
+
 # Remove VPN
 $VPNName = "AOVPN" # Write down the VPN Name as shown in the "Name" output.
 Get-VPNConnection -Name $VPNName | Remove-VPN
