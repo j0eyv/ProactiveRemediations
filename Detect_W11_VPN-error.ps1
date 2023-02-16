964ad96d64d05f6aa9e5b42d12b5ee4fbcac43e2
j0eyv <41282854+j0eyv@users.noreply.github.com>
2023-02-16 13:31:28 +0100

    Add files via upload

---
+++ Detect_W11_VPN-error.ps1
@@ -0,0 +1,18 @@
+﻿# Windows 11 and Intune are no a great combination. When deploying VPN connections to Windows 11 via Intune, they get corrupt. The command get-vpnconnection does result in a strange error.
+# Ths Proactive Remediation script checks if the VPN Profile is available. If not, an error will show and will trigger the remediation.
+
+$VPNName = "AOVPN" # Write down the VPN Name as shown in the "Name" output.
+
+Try {
+    $GETVPN = Get-VPNConnection -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Name
+    If ($GetVPN -eq $VPNName){
+        Write-Output "Compliant"
+        Exit 0
+    } 
+    Write-Warning "Not Compliant"
+    Exit 1
+} 
+Catch {
+    Write-Warning "Not Compliant"
+    Exit 1
+}
---
+++ Remediate_W11_VPN-error.ps1
@@ -0,0 +1,10 @@
+﻿# The remediation will remove the VPN connection and trigger a Intune sync.
+# VP Connection should be available again in a few minutes.
+
+# Remove VPN
+$VPNName = "AOVPN" # Write down the VPN Name as shown in the "Name" output.
+Get-VPNConnection -Name $VPNName | Remove-VPN
+
+
+# Force Sync Intune
+Get-ScheduledTask | ? {$_.TaskName -eq 'Login Schedule created by enrollmet client'} | Start-ScheduledTask
